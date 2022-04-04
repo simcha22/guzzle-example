b6ab2cb1e7b2afdf72ae1058ce01033a5ff5e3d9
simcha fridman <simcha1095@gmail.com>
2022-04-04 15:34:11 +0300

    response

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,29 +20,57 @@ class LoginController extends Controller
         $user = User::where('user_name', $request->user_name)
             ->where('password', md5($request->user_name . $request->password))
             ->first();
+
         //$user = DB::select("select * from pa.pa_users where password = (select md5('" . $request->user_name . $request->password . "'))");
 
         if (!$user) {
-            return response([
-                'message' => 'Bad creds'
-            ], 401);
-        } 
+            return response()->json([
+                'clientAddress' => $request->ip(),
+                'message' => "Invalid Username or password",
+                'otpEnabled' => null,
+                'phone' => null,
+                'result' => "REJECTED",
+                'userGroupId' => null,
+                'userId' => 0,
+                'userLevel' => null,
+                'userType' => null,
+                'username' => null,
+            ], 200);
+        }
+
+        // Check user group
+        $group = DB::table('pa.pa_user_to_groups')->select('ug_id')->where('u_id', $user->id)->first();
 
         $token = $user->createToken('myapptoken')->plainTextToken;
 
         $response = [
-            'user' => $user,
+            'clientAddress' => $request->ip(),
+            'message' => null,
+            'otpEnabled' => $user->otp_enabled,
+            'phone' => $user->phone_number,
+            'result' => "SUCCESS",
+            'userGroupId' => $group->ug_id,
+            'userId' => $user->id,
+            'userLevel' => $user->user_type,
+            'userType' => $user->user_type,
+            'username' => $user->user_name,
+            //'user' => $user,
             'token' => $token
         ];
 
-        return response($response, 201);
+        return response()->json($response, 201);
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
 
         $request->user()->currentAccessToken()->delete();
-        return [
-            'message' => 'Logged out'
+        $response = [
+            'clientAddress' => $request->ip(),
+            'message' => null,
+            'result' => "SUCCESS",
         ];
+
+        return response()->json($response, 200);
     }
 }
